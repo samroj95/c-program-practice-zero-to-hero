b44336671e38054afbab161bfa6b5908c298a275
samroj95 <92337579+samroj95@users.noreply.github.com>
2022-06-26 00:31:38 -1200

    Add files via upload

---
+++ hello world.c
@@ -0,0 +1,15 @@
+//for Gcc compiler... 
+#include<stdio.h>
+int main(){
+    printf("Hello world..");
+    return 0;
+}
+/*if You use Turbo c then you have write this code because turbo gives output on consol window..*/
+#include<stdio.h>
+#include<conio.h>
+int main(){
+    clrscr();
+    printf("Hello world..");
+    getch();
+    return 0;
+}
