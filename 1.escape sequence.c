917572ea2768bcec717d3152e75986f34a0d517e
samroj95 <92337579+samroj95@users.noreply.github.com>
2022-06-26 00:52:50 -1200

    Add files via upload

---
+++ 1.escape sequence.c
@@ -0,0 +1,10 @@
+//for Gcc compiler... 
+//escape sequence....
+#include<stdio.h>
+int main(){
+    printf("Amit kumar\nsumit kumar");//(\n) use for new line..
+    return 0;
+}
+/*output
+Amit kumar
+sumit kumar*/
